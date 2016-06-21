8c387b50d80b0c7c8753d8b9d98cfa1ee9453903
Fahad Wajed <fahadwajed@gmail.com>
2016-06-21 12:01:08 +0600

    adds main activity

---
+++ app/src/main/java/co/gobd/tracker/ui/MainActivity.java
@@ -0,0 +1,22 @@
+package co.gobd.tracker.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import co.gobd.tracker.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+        }
+    }
+}
